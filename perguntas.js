cfaf12f548f7756674b0b39faf07501bc3ae314b
Ana Jú. <00001106241903sp@al.educacao.sp.gov.br>
2024-11-25 14:28:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Histótia',
+    'Quem foi Getúlio Vargas?',
+    'Getúlio Vargas foi um político brasileiro importante que governou o Brasil'
+)
+
+criaCartao(
+    'Geografia',
+    'O que significa a sigla ONU?',
+    'Significa Organização das Nações Unidas'
+)
+
+criaCartao(
+    'Português',
+    'Os Lusíadas é de qual escola literária?',
+    'É uma obra que pertence ao Renascimento'
+)
+
+criaCartao(
+    'Lingua italiana',
+    'Como se diz "boa tarde" em italiano?',
+    'Boa tarde em italiano é: Buon pomeriggio'
+)
